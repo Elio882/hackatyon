ced7f56b31b97fbfe552fbb0781f3641db70beec
Mauricio Elio Flores Herbas <mfloresherbas00@gmail.com>
2024-11-08 05:53:09 -0400

    plugin

---
+++ probandomibackend/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from 'server';
+import configuration from './config/configuration';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI='mongodb+srv://mfloresherbas00:GvFvXCzbEQ74Ptmv@cluster0.v3ckw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ probandomibackend/src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { bold } from 'chalk';
+import { RequestMethod, VersioningType } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { configSwagger } from './helpers/swagger.helper';
+//import { GlobalValidationPipe } from 'blade-core';
+import { urlencoded, json } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
+  const envService = app.get(ConfigService);
+  const packageJson = envService.get('packageJson');
+  app.use(json({ limit: envService.get('limitRequest') }));
+  app.use(urlencoded({ extended: true, limit: envService.get('limitRequest') }));
+  app.enableVersioning({ type: VersioningType.URI });
+  app.use(compression());
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
+
+  //app.useGlobalPipes(new GlobalValidationPipe());
+  app.enableCors({
+    origin: envService.get('enableCors'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  if (envService.get('environment') !== 'production') {
+    configSwagger(app, packageJson);
+  }
+  const port = envService.get<number>('port');
+  await app.listen(port, '0.0.0.0').then(async () => {
+    console.info(
+      bold.blue(
+        `🚀 "${packageJson.name}", version:"${packageJson.version}" API is listening ON PORT`,
+        `${await app.getUrl()}/api`,
+      ),
+    );
+  });
+}
+bootstrap();
